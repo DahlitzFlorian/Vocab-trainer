e6690e39a2cda0bdf7d83b56b4137cf3c73f7302
florian dahlitz <f2dahlitz@freenet.de>
2017-04-14 11:40:18 +0200

    document the main-files

--- vocab_trainer.py
+++ vocab_trainer.py
@@ -1,41 +1,56 @@
+"""Vocab-Trainer
+
+    This application is a lightweighted easy to handle
+    vocab-trainer written in Python 3.4
+
+    @author Florian Dahlitz
+"""
 import vocab_trainer_fct as voc
 import os
 import re
 
 
+# getting the path to the file storing the vocab to train on
 path = voc.getPath("vocab_settings.txt")
+
+# receiving mode-info and vocab-pairs
 vocab_info = voc.getFromFile(path)
 vocab = vocab_info[0]
+
+# setting up the field to play on
 userInput = None
-field = {i : None for i in range(1, 26)}    # create field with default: no vocab
-controls = (3, 8, 13, 18, 25)   # places, where user is asked for translation
+field = {i : None for i in range(1, 26)}
+controls = (3, 8, 13, 18, 25)
+
+# creating vocab-lists
 numberEntireVocab = len(vocab)
 vocabLeft = numberEntireVocab
 vocabFinished = 0
 
+# adding forms input if the mode was selected
 if vocab_info[1] == True:
     userFormsInput = None
 
-
+# creats the window
 os.system("cls")
 print("{} finished. {} left. Entire: {}".format(vocabFinished, vocabLeft, numberEntireVocab))
 print("-"*50)
 
+# training on vocab
 while vocabLeft != 0:
-
     referencedField = 25
-
     while(referencedField > 0):
-
+        # check if vocab is on the referenced field
         if field[referencedField] != None:
-
             os.system("cls")
             print("{} finished. {} left. Entire: {}".format(vocabFinished, vocabLeft, numberEntireVocab))
             print("-"*50)
 
+            # checks if the referenced field is a controlling field
             if referencedField in controls:
                 currVocab = field[referencedField] # currVocab is tuple
                 
+                # takes the vocab on the referenced field
                 vocabKey = currVocab[0]
                 keys = ', '.join(map(str, vocabKey))
                 print(keys)
@@ -48,44 +63,35 @@ while vocabLeft != 0:
 
                 ##############################################
                 # should remove everything in brackets such as (to)
-
                 valueList = []
-
                 for value in valueTup:
-
                     valueList.append(value)
 
                 valueTupUser = []   # first it will be a list to be mutable
-    
                 for value in valueList:
                     value = re.sub(r'\(.*?\)\ *', '', value)
                     valueTupUser.append(value)
 
                 valueTupUser = tuple(valueTupUser)
-
                 # end of it
                 #############################################
-
+                
+                # asks for forms input if mode was selected
                 if vocab_info[1] == True:
                     userFormsInput = input("Forms: ")
-
                     userFormsInput = userFormsInput.split(", ")
                     userFormsList = []
-
                     for form in userFormsInput:
                         userFormsList.append(form)
 
                     userFormsInput = tuple(userFormsList)
-
                     userInput = input("Translation: ")
-
                     if (userInput in valueTupUser) and (formsTupUser == userFormsInput):
                         if referencedField == 25:
                             field[referencedField] = None
                             vocabLeft -= 1
                             vocabFinished += 1
                         else:
-
                             all_forms = formsTupUser
                             values = valueTupUser
                             print("-"*50)
@@ -95,7 +101,6 @@ while vocabLeft != 0:
 
                             field[referencedField + 1] = currVocab
                             field[referencedField] = None
-
                     else:
                         all_forms = formsTupUser
                         values = valueTupUser
@@ -105,9 +110,9 @@ while vocabLeft != 0:
                         os.system("pause")
                         vocab[vocabKey] = (formsTupUser, valueTup)
                         field[referencedField] = None
+                # does not asks for forms if mode was not selected
                 else:
                     userInput = input("Translation: ")
-
                     if userInput in valueTupUser:
                         if referencedField == 25:
                             field[referencedField] = None
@@ -121,7 +126,6 @@ while vocabLeft != 0:
 
                             field[referencedField + 1] = currVocab
                             field[referencedField] = None
-
                     else:
                         all_forms = formsTupUser
                         values = valueTupUser
@@ -131,9 +135,10 @@ while vocabLeft != 0:
                         os.system("pause")
                         vocab[vocabKey] = valueTup
                         field[referencedField] = None
-
+            # starting point -> displays the vocab-pair
             elif referencedField == 1:
                 field[referencedField+1] = field[referencedField]
+                # get new pair
                 if bool(vocab) == True:
                     randomKey = voc.getRandom(vocab)
                     for tup in vocab:
@@ -148,18 +153,17 @@ while vocabLeft != 0:
                         print("{} - {}".format(", ".join(randomKey), ", ".join(vocab[randomKey])))
 
                     input("Press Enter.")
-
                     del vocab[randomKey]
+                # None if vocab is empty
                 else:
                     field[referencedField] = None
-
             else:
                 field[referencedField+1] = field[referencedField]
                 field[referencedField] = None
-        
             referencedField -= 1
-
+        # get new vocab pair
         elif referencedField == 1:
+            # new pair
             if bool(vocab) == True:
                 randomKey = voc.getRandom(vocab)
                 for tup in vocab:
@@ -174,14 +178,12 @@ while vocabLeft != 0:
                 input("Press Enter.")
 
                 del vocab[randomKey]
+            # None if vocab is empty
             else:
                 field[referencedField] = None
-
             referencedField -= 1
-
         else:
             referencedField -= 1
-
+    # closes application
     if (userInput == "0") or (userFormsInput == "0"):
-
         vocabLeft = 0
--- vocab_trainer_fct.py
+++ vocab_trainer_fct.py
@@ -1,51 +1,59 @@
+"""Vocab-Trainer functions
+
+    This file provides functions for the vocab-trainer 
+    application.
+
+    @author Florian Dahlitz
+"""
 import random
 
 
 def getPath(settingsFile) :
+    """Get path
 
+        Returns the path of the file containing the vocab-pairs.
+    """
     fullPath = "C:/Users/Example/" # add full path to dir with files here
     settings = {}
     settFile = open(fullPath + settingsFile, "r")
-
     for line in settFile :
-
         line = line.strip()
         parts = line.split(" ")
         settings[parts[0]] = parts[1]
-    
     settFile.close()
 
     return fullPath + settings["path"]
 
-def getFromFile(filePath, reverse = False) :
 
-    words = {}  # must be a dict including key=tuple, value=tuple
+def getFromFile(filePath, reverse = False) :
+    """Get from file
+
+        Returns the vocab-pairs and the mode.
+    """
+    # must be a dict including key=tuple, value=tuple
+    words = {}
     is_latin = False
 
+    # reads in with forms
     if "latin" in filePath:
         is_latin = True
-
         file = open(filePath, "r")
-
-        for line in file :
-
+        for line in file:
             line = line.strip()
             parts = line.split(" - ")
             lang1 = tuple(parts[0].split(", "))
             forms = tuple(parts[1].split(", "))
             lang2 = tuple(parts[2].split(", "))
-
-            if reverse == False :
+            if reverse == False:
                 words[lang1] = (forms,lang2)
-            else :
+            else:
                 words[lang2] = lang1
     
         file.close()
+    # reads in without forms
     else:
         file = open(filePath, "r")
-
         for line in file:
-
             line = line.strip()
             parts = line.split(" - ")
             lang1 = tuple(parts[0].split(", "))
@@ -59,15 +67,20 @@ def getFromFile(filePath, reverse = False) :
     
     return (words, is_latin)
 
+
 def getRandom(words, wordsReverse = False) :
+    """Get random
 
-    if wordsReverse == False :
+        Returns a random key of the given vocab-list.
+    """
+    # choosing without reverse vocab
+    if wordsReverse == False:
         return random.choice(list(random.choice(list(words.keys()))))
-    else :
+    # choosing with reverse vocab
+    else:
         choice = random.randint(0,1)
-
-        if choice == 0 :
+        if choice == 0:
             return random.choice(list(random.choice(list(words.keys()))))
-        else :
+        else:
             return random.choice(list(random.choice(list(wordsReverse.keys()))))
 
